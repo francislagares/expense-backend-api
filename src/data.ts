24415ea801b67e91cce73b7a921385ad553bf420
Francis Lagares <francis.lagares@gmail.com>
2022-02-27 21:19:50 +0100

    feat: add sample data

---
+++ src/data.ts
@@ -0,0 +1,44 @@
+interface IData {
+  report: {
+    id: string;
+    source: string;
+    amount: number;
+    created_at: Date;
+    updated_at: Date;
+    type: ReportType;
+  }[];
+}
+
+export enum ReportType {
+  INCOME = 'income',
+  EXPENSE = 'expense',
+}
+
+export const data: IData = {
+  report: [
+    {
+      id: 'uuid1',
+      source: 'Salary',
+      amount: 7500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid2',
+      source: 'YouTube',
+      amount: 2500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.INCOME,
+    },
+    {
+      id: 'uuid3',
+      source: 'Fook',
+      amount: 500,
+      created_at: new Date(),
+      updated_at: new Date(),
+      type: ReportType.EXPENSE,
+    },
+  ],
+};
