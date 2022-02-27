fa3427f58b0104115b96d4ecf938392baf7b384d
Francis Lagares <francis.lagares@gmail.com>
2022-02-27 21:21:30 +0100

    feat: implement getAllReports method filtered by type

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,32 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Delete, Get, Post, Put, Param } from '@nestjs/common';
+import { data, ReportType } from './data';
 
-@Controller()
+@Controller('/report/:type')
 export class AppController {
-  constructor(private readonly appService: AppService) {}
-
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getAllReports(@Param('type') type: string) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+    return data.report.filter((report) => report.type === reportType);
+  }
+
+  @Get('/:id')
+  getReportById() {
+    return {};
+  }
+
+  @Post()
+  createReport() {
+    return 'Created';
+  }
+
+  @Put('/:id')
+  updateReport() {
+    return 'Updated';
+  }
+
+  @Delete('/:id')
+  deleteReport() {
+    return 'Deleted';
   }
 }
