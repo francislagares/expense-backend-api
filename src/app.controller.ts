cdac3a6bd50c43895685f3325f7c0f110a4c4aac
Francis Lagares <francis.lagares@gmail.com>
2022-03-03 21:16:06 +0100

    feat: implement updateReport method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,4 @@
-import {
-  Controller,
-  Delete,
-  Get,
-  Post,
-  Put,
-  Param,
-  Body,
-} from '@nestjs/common';
+import { Controller, Get, Post, Put, Param, Body } from '@nestjs/common';
 import { data, ReportType } from './data';
 import { v4 as uuid } from 'uuid';
 
@@ -46,5 +38,32 @@ export class AppController {
     data.report.push(newReport);
 
     return newReport;
+  }
+
+  @Put('/:id')
+  updateReport(
+    @Param('type') type: string,
+    @Param('id') id: string,
+    @Body() body: { amount: number; source: string },
+  ) {
+    const reportType =
+      type === 'income' ? ReportType.INCOME : ReportType.EXPENSE;
+
+    const reportToUpdate = data.report
+      .filter((report) => report.type === reportType)
+      .find((report) => report.id === id);
+
+    if (!reportToUpdate) return;
+
+    const reportIndex = data.report.findIndex(
+      (report) => report.id === reportToUpdate.id,
+    );
+
+    data.report[reportIndex] = {
+      ...data.report[reportIndex],
+      ...body,
+    };
+
+    return data.report[reportIndex];
   }
 }
